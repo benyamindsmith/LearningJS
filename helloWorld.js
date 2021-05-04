e3dbf9db1761f34e00dd98075aedf5cdcb845650
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-05-03 20:37:32 -0400

    Don't make fun of me!!

---
+++ helloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+function Greeting() {
+    return <h4>
+        Hello World!
+    </h4>;         
+}
+
+ReactDom.render(<Greeting />, document.getElementById('root'));
