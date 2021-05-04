4d14ee8d52c0cf96225b00e8522bf63ae1f11867
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-05-03 20:47:10 -0400

    Update helloWorld.js

--- helloWorld.js
+++ helloWorld.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+/*
+ For this to display on your server your function needs to be capialized.
+ */ 
+
 function Greeting() {
     return <h4>
         Hello World!
