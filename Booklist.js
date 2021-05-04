35ddb3ecba4540e6eac4fd7664bc58e84a4738e8
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-05-04 13:12:22 -0400

    This is a milklist 🥛

--- Booklist.js
+++ Booklist.js
@@ -1,48 +1,41 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
 
 /*
  For this to display on your server your function needs to be capialized.
- */ 
+ */
 
 function BookList() {
-    return (
-        <section>
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-            <Image />
-            <Book />
-        </section>  
-
-    );         
+  return (
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
+  );
 }
 
 const Book = () => {
-    return (
-        <h1>
-            This is a milk
-        </h1>);
+  return (
+    <article>
+      <Title />
+      <Image />
+    </article>
+  );
 };
-
+const Title = () => {
+  return <h1>This is Milk</h1>;
+};
 const Image = () => {
-    return (
-        <img src="https://ychef.files.bbci.co.uk/976x549/p07vvrpj.jpg" alt=""/>
-        );
-}
-
-ReactDom.render(<BookList />, document.getElementById('root'));
+  return (
+    <img src="https://ychef.files.bbci.co.uk/976x549/p07vvrpj.jpg" alt="" />
+  );
+};
+ReactDom.render(<BookList />, document.getElementById("root"));
