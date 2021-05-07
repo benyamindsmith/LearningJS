3fe97de45a6505a9f779885f84312693771f644a
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-05-06 20:36:04 -0400

    Update eventsTutorial.js

--- node/eventsTutorial.js
+++ node/eventsTutorial.js
@@ -1,3 +1,5 @@
+// Basic Idea of Events
+
 const EventEmitter = require("events");
 const eventEmitter = new EventEmitter();
 
@@ -11,3 +13,37 @@ eventEmitter.on("sum", (num1, num2) => {
 
 eventEmitter.emit("tutorial");
 eventEmitter.emit("sum", 1, 2);
+
+// Application
+// First lets make a class
+class Person extends EventEmitter {
+  // Then define a constructor
+  constructor(name) {
+    //super allow us to use the `this` property
+    super();
+    this._name = name;
+  }
+  // Define a method
+  get name() {
+    return this._name;
+  }
+}
+
+// Now lets impliment it
+// First define a Person
+let ben = new Person("Ben");
+let shev = new Person("Shev");
+
+// Now, define an event
+ben.on("name", () => {
+  console.log("My name is " + ben._name);
+});
+
+shev.on("name", () => {
+  console.log("My name is " + shev._name);
+});
+
+// Call the event for the
+ben.emit("name");
+shev.emit("name");
+
