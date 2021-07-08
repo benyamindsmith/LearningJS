de5f438bcc1b64ef4d881f3257b781597ac79e1a
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-07-08 18:30:45 -0400

    Variable Types

---
+++ Tutorials/ReadMe.md
@@ -0,0 +1,3 @@
+# Tutorials
+
+A folder of codes I developed while learning Javascript with [FreeCodeCamp's video](https://www.youtube.com/watch?v=PkZNo7MFNFg&t=1687s) on the topic
---
+++ Tutorials/variableDefinitions.js
@@ -0,0 +1,12 @@
+// There are a few ways we can define variables
+
+// var will be able to be used throughout our whole program
+
+var myName = "Ben";
+
+// let will only be used within the scope which it is declared
+
+let ourName = "FreeCodeCamp";
+
+// const is for a variable that should never change and will never change
+const pi = 3.14;
