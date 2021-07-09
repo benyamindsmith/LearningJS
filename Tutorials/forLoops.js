c94a9ec067960b05f7daf3697b781405bbcddd7e
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-07-09 16:39:10 -0400

    for loops

--- Tutorials/forLoops.js
+++ Tutorials/forLoops.js
@@ -5,3 +5,30 @@ for (var i = 0; i < 5; i++) {
 }
 
 console.log(myArray);
+
+// Another example
+var numbers = [9, 5, 3, 1, 4];
+var myTotal = 0;
+// Add up the numbers
+for (var i = 0; i < numbers.length; i++) {
+  myTotal += numbers[i];
+}
+
+console.log(myTotal);
+
+// Nested for loops
+
+var numbersList = [
+  [1, 2],
+  [3, 4, 5],
+  [6, 7, 8, 9],
+];
+var product = 0;
+
+for (var i = 0; i < numbersList.length; i++) {
+  for (var j = 0; j < numbersList[i].length; j++) {
+    product *= numbersList[i][j];
+  }
+}
+
+console.log(product);
