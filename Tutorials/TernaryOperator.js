6aff496703bd6ed6c97cf0b8633ecfd130cab11e
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-07-09 17:09:43 -0400

    Ternary Operator

---
+++ Tutorials/TernaryOperator.js
@@ -0,0 +1,9 @@
+function theWinner(a, b) {
+  // Question mark is the Ternary Operator
+  return a > b ? "A is bigger" : "B is equal to a or bigger";
+}
+
+console.log(theWinner(4, 2));
+
+console.log(theWinner(1, 2));
+console.log(theWinner(1, 1));
