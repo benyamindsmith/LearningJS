82586c47973891e2438081aa186db1ebac467c38
Benjamin Smith <46410142+benyamindsmith@users.noreply.github.com>
2021-07-08 23:26:38 -0400

    Append Actions

---
+++ Tutorials/appendActions.js
@@ -0,0 +1,35 @@
+// Append Elements to an array
+
+var myArray = ["You", "like"];
+// to append elements to an array use the .push() method
+
+myArray.push(["to", "Drink", "Milk"]);
+
+// returns the new array in the array
+
+console.log(myArray);
+
+var anotherArray = ["You", "Like"];
+
+// Appends new element to the array
+anotherArray.push("Milk");
+
+console.log(anotherArray);
+
+// .pop() removes the last element from the array
+
+anotherArray.pop();
+
+console.log(anotherArray);
+
+// .shift removes the first element
+
+anotherArray.shift();
+
+console.log(anotherArray);
+
+// .unshift adds an Element to the beggining of an array
+
+anotherArray.unshift("Bruh");
+
+console.log(anotherArray);
---
+++ Tutorials/functions.js
@@ -0,0 +1,7 @@
+function myFunction(name) {
+  var result = "Hello " + name;
+
+  return result;
+}
+
+console.log(myFunction("Timmy"));
